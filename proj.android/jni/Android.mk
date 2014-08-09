6b119cde17718788f339d9863f7ed76dba454b16
Patrick Hoey <patrick.hoey@gmail.com>
2014-08-09 14:06:43 -0700

    Got the build working on Android (reverted from commit 8254e8922a27f39a72a350083e7cae6f14bb3ce9)

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -5,22 +5,14 @@ include $(CLEAR_VARS)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
-$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/base)
-
 
 LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
-           ../../Classes/AppDelegate.cpp \
-           ../../Classes/GameEnd.cpp \
-           ../../Classes/Grid.cpp \
-           ../../Classes/MainScene.cpp \
-           ../../Classes/OptionMenu.cpp \
-           ../../Classes/Setting.cpp \
-           ../../Classes/Tile.cpp \
-           ../../Classes/TitleMenu.cpp
+                   ../../Classes/AppDelegate.cpp \
+                   ../../Classes/HelloWorldScene.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
@@ -29,7 +21,6 @@ LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 
 # LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
-LOCAL_WHOLE_STATIC_LIBRARIES += spritebuilder_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += cocos_network_static
@@ -43,7 +34,6 @@ $(call import-module,audio/android)
 
 # $(call import-module,Box2D)
 # $(call import-module,editor-support/cocosbuilder)
-$(call import-module,editor-support/spritebuilder) 
 # $(call import-module,editor-support/spine)
 # $(call import-module,editor-support/cocostudio)
 # $(call import-module,network)
