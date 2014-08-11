ec32dd2ef336e27989750c7a68d3bdec308b3ead
Patrick Hoey <patrick.hoey@gmail.com>
2014-08-10 17:34:37 -0700

    Created build script of Android

---
+++ proj.android/build.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+declare -x ANT_ROOT="/Users/phoey/Development/apache-ant-1.9.4/bin"
+declare -x ANDROID_SDK_ROOT="/Users/phoey/Development/android-sdk-macosx"
+declare -x NDK_ROOT="/Users/phoey/Development/android-ndk-r9d"
+cocos compile -p android
