dc4e9fb8dfc3034e6da3537b8c21315ad59f08f5
Patrick Hoey <patrick.hoey@gmail.com>
2014-08-15 17:25:11 -0700

    Project changes

--- proj.android/build.sh
+++ proj.android/build.sh
@@ -1,6 +1,6 @@
 #/bin/sh
-declare -x ANT_ROOT="/Users/phoey/Development/apache-ant-1.9.4/bin"
-declare -x ANDROID_SDK_ROOT="/Users/phoey/Development/adt-bundle-mac-x86_64-20140702/sdk"
-declare -x NDK_ROOT="/Users/phoey/Development/android-ndk-r9d"
-#cocos compile -p android -m debug --ndk-mode NDK_DEBUG=1
-cocos compile -p android -m release --ndk-mode NDK_DEBUG=0
+declare -x ANT_ROOT="${HOME}/Dev/apache-ant-1.9.4/bin"
+declare -x ANDROID_SDK_ROOT="${HOME}/Dev/adt-bundle-mac-x86_64-20140702/sdk"
+declare -x NDK_ROOT="${HOME}/Dev/android-ndk-r9d"
+cocos compile -p android -m debug --ndk-mode NDK_DEBUG=1
+#cocos compile -p android -m release --ndk-mode NDK_DEBUG=0
